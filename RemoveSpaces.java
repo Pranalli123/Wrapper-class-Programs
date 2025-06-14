cf399a80ccffa58e4c1e9ec3f1bf7d41e54ff2d7
Pranali Shinde <shindepranali466@gmail.com>
2025-06-14 11:39:32 +0530

    Add files via upload

---
+++ RemoveSpaces.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class RemoveSpaces
+{
+ public static void main(String x[])
+ {
+  String input="hello world";
+  String result=input.replaceAll("","");
+  System.out.println("String without spaces:" + result);
+}
+}
